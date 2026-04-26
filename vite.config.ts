4c105a6dd8cbb201a401a8a7d21d65b729c09184
phunkhazardcrew-svg <phunkhazardcrew@gmail.com>
2026-04-26 08:33:21 +0000

    fix: Remove terser dependency, use default esbuild minifier

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  },
-  resolve: {
-    alias: { '@': '/src' },
-  },
+  build: { outDir: 'dist', sourcemap: false },
+  resolve: { alias: { '@': '/src' } },
 });
